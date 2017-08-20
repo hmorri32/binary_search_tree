50379effdbad4433b808d9147d41840e098644f5
Hugh Morrison <hmorri32@gmail.com>
2017-08-20 16:50:12 -0600

    write recursive include? method

--- lib/tree.rb
+++ lib/tree.rb
@@ -12,4 +12,16 @@ class Tree
     head.nil? ? (@head = Node.new(score, title); head.depth) 
               :  @head.insert_node(score, title)
   end
+
+  def include?(score)
+    return true if @head.score == score
+
+    if (@head.score < score) && @head.right != nil
+      include?(score)
+    elsif (@head.score > score) && @head.left != nil
+      include?(score)
+    else
+      return false
+    end
+  end
 end
