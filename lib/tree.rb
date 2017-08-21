7453256c9b65db8a0887ccdfa98aad5bc0378a42
Hugh Morrison <hmorri32@gmail.com>
2017-08-21 15:16:05 -0600

    refactor gross ternary, fix recursive include method

--- lib/tree.rb
+++ lib/tree.rb
@@ -8,20 +8,44 @@ class Tree
     @head = nil
   end
 
-  def insert(score, title)
-    head.nil? ? (@head = Node.new(score, title); head.depth) 
-              :  @head.insert_node(score, title)
+  def insert(score, title) #cant read this. 
+    if head.nil? 
+      @head = Node.new(score, title)
+      head.depth
+    else
+      @head.insert_node(score, title)
+    end
   end
 
-  def include?(score)
-    return true if @head.score == score
+  def traverse_left?
+  end
 
-    if (@head.score < score) && @head.right != nil
-      include?(score)
-    elsif (@head.score > score) && @head.left != nil
-      include?(score)
+  def traverse_right?
+  end
+
+  def include?(score, node = @head)
+    return true if node.score == score
+
+    if (node.score < score) && node.right
+      include?(score, node.right) # pass score node.right
+
+    elsif (node.score > score) && node.left
+      include?(score, node.left)
     else
       return false
     end
+  end
+
+  def depth_of(score, node = @head)
+    # TODO
+
+    if @head.score > score && @head.left
+      
+    end
+
+    if @head.score < score && head.right
+
+    end
+
   end
 end
