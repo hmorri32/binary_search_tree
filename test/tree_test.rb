7d6cf04871d96bf9769fad8fffb8848b6befbcc4
Hugh Morrison <hmorri32@gmail.com>
2017-08-21 15:15:38 -0600

    fix spacing in testing suite, fix include test, write basic depth_of tests

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -22,12 +22,14 @@ class TreeTest < Minitest::Test
 
   def test_can_insert_movie
     @tree.insert(20, 'cool movie')
+
     assert_equal 20, @tree.head.score
     assert_equal 'cool movie', @tree.head.title
   end
 
   def test_depth_zero_after_first_insertion
     @tree.insert(20, 'cool movie')
+
     assert_equal 20, @tree.head.score
     assert_equal 0, @tree.head.depth
   end
@@ -35,17 +37,22 @@ class TreeTest < Minitest::Test
   def test_depth_after_two
     @tree.insert(20, 'sharknado')    
     @tree.insert(25, 'lebowski')
+
     assert_equal 1, @tree.head.right.depth
   end
 
   def test_insert_returns_depth
     assert_equal 0, @tree.insert(20, 'coool')
     assert_equal 1, @tree.insert(10, 'coool')
+    assert_equal 2, @tree.insert(8, 'another')
+    assert_equal 2, @tree.insert(12, 'another')
   end
 
   def test_cant_insert_same_score
     @tree.insert(20, 'sharknado')    
     @tree.insert(20, 'lost')
+
+    assert_equal "sharknado", @tree.head.title
     assert_nil @tree.head.left
     assert_nil @tree.head.right
   end
@@ -53,8 +60,26 @@ class TreeTest < Minitest::Test
   def test_include_returns_bool
     @tree.insert(20, 'sharknado')    
     @tree.insert(25, 'lebowski')
+    @tree.insert(40, 'cool')
+
     assert_equal false, @tree.include?(10)
     assert_equal true, @tree.include?(20)
+    assert_equal true, @tree.include?(40)
+  end
+
+  def test_depth_of_one_movie
+    skip
+    @tree.insert(30, 'airplane')
+    assert_equal 0, @tree.depth_of(30)
+  end
+
+  def test_depth_heaps_of_flicks
+    skip
+    @tree.insert(64, "Zoolander")
+    @tree.insert(95, "This is Spinal Tap")
+    @tree.insert(74, "Caddyshack")
+    @tree.insert(98, "Monty Python")
+    assert_equal 1, @tree.depth_of(64)
   end
 
 end
