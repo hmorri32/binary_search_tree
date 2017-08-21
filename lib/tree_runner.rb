3b34cb81aa5d2a1228cf35ade2a34e0abc3eaccf
Hugh Morrison <hmorri32@gmail.com>
2017-08-21 15:15:10 -0600

    insert && depth methods in runner

--- lib/tree_runner.rb
+++ lib/tree_runner.rb
@@ -1,3 +1,12 @@
 require_relative 'node'
 require_relative 'tree'
 require 'pry'
+
+
+tree = Tree.new
+
+tree.insert(25, "Zoolander")
+tree.insert(10, "This is Spinal Tap")
+tree.insert(14, "Caddyshack")
+tree.insert(30, "Monty Python")
+# tree.depth_of(10)
