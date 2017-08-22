894070917506e73514e87ee2fe240a134694a0a9
Hugh Morrison <hmorri32@gmail.com>
2017-08-22 11:50:48 -0600

    clear runner

--- lib/tree_runner.rb
+++ lib/tree_runner.rb
@@ -1,12 +1,3 @@
 require_relative 'node'
 require_relative 'tree'
 require 'pry'
-
-
-tree = Tree.new
-
-tree.insert(25, "Zoolander")
-tree.insert(10, "This is Spinal Tap")
-tree.insert(14, "Caddyshack")
-tree.insert(30, "Monty Python")
-# tree.depth_of(10)
