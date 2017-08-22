069a425d9d03046130a394997ce80636d3d13bc9
Hugh Morrison <hmorri32@gmail.com>
2017-08-22 14:26:49 -0600

    health tree_runner

--- lib/tree_runner.rb
+++ lib/tree_runner.rb
@@ -1,3 +1,12 @@
 require_relative 'node'
 require_relative 'tree'
 require 'pry'
+
+tree = Tree.new
+tree.insert(64, "Zoolander")
+tree.insert(95, "This is Spinal Tap")
+tree.insert(74, "Caddyshack")
+tree.insert(98, "Monty Python")
+tree.insert(12, "nested dirty")
+
+tree.health(0)
