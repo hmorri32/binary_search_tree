5427bc8b47cf39ef9a5634b929db536276713124
Hugh Morrison <hmorri32@gmail.com>
2017-08-21 20:13:42 -0600

    refactor include? depth_of write sort & load

--- lib/tree.rb
+++ lib/tree.rb
@@ -17,12 +17,20 @@ class Tree
     end
   end
 
+  def right_node?(score, node)
+    node.score < score && node.right
+  end
+  
+  def left_node?(score, node)
+    node.score > score && node.left
+  end
+  
   def include?(score, node = @head)
     return true if node.score == score
 
-    if node.score < score && node.right
+    if right_node?(score, node)
       include?(score, node.right) 
-    elsif node.score > score && node.left
+    elsif left_node?(score, node)
       include?(score, node.left)
     else
       return false
@@ -32,9 +40,9 @@ class Tree
   def depth_of(score, node = @head)
     return node.depth if node.score === score
 
-    if node.score < score && node.right
+    if right_node?(score, node)
       depth_of(score, node.right)
-    elsif node.score > score && node.left
+    elsif left_node?(score, node)
       depth_of(score, node.left)
     else
       nil
@@ -49,7 +57,41 @@ class Tree
     node.left.nil? ? node.movie_obj : min(node.left)
   end
 
-  def sort
+  def sort(node = @head)
+    sorted = []
+    branch_sort(node, sorted)
     # sort pls
+    # sorted.push(node.movie_obj)   if node.left.nil?
+    # sorted.push(sort(node.left))  if !node.left.nil?
+    # sorted.push(node.movie_obj)   if !sorted.include?(node.movie_obj)
+    # sorted.push(sort(node.right)) if !node.right.nil?
+    sorted
+  end
+  
+  def branch_sort(node, arr)
+    return if node.nil?
+    branch_sort(node.left, arr)
+    arr.push(node.movie_obj)
+    branch_sort(node.right, arr)
+  end
+
+  def load(file)
+    total = 0
+    File.read(file).each_line do |movie|
+      clean = format_flick(movie)
+      insert_loaded_movie(clean)
+      total += 1
+    end
+    total
+  end
+
+  def format_flick(movie)
+    movie.strip.split(", ")      
+  end
+
+  def insert_loaded_movie(movie)
+    score = movie.first.to_i
+    title = movie.last
+    insert(score, title) 
   end
 end
